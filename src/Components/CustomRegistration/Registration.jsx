0aa39e5695bf70bdba7dde6bb098169db0e5cb8f
Mohit Saxena <Mohit.Saxena1@Geminisolutions.com>
2023-06-21 19:30:38 +0530

    final code

---
+++ src/Components/CustomRegistration/Registration.jsx
@@ -0,0 +1,284 @@
+import React,{useState} from 'react'
+
+import Navbar from '../CustomNavBar/Navbar'
+
+export default function Registration() {
+
+    const [username, setUsername] = useState("");
+
+    const [password, setPassword] = useState("");
+
+    const [name, setName] = useState("");
+
+
+
+    const handleUsernameChange = (e) => {
+
+        setUsername(e.target.value);
+
+    };
+
+
+
+
+    const handlePasswordChange = (e) => {
+
+        setPassword(e.target.value);
+
+    };
+
+
+
+
+    const handleNameChange = (e) => {
+
+        setName(e.target.value);
+
+    };
+
+
+
+
+    // const handleEmailChange = (e) => {
+
+    //   setEmail(e.target.value);
+
+    // };
+
+
+
+
+    // const handleMobileChange = (e) => {
+
+    //   setMobile(e.target.value);
+
+    // };
+
+
+
+
+    const handleRegister = (e) => {
+
+        e.preventDefault();
+
+
+
+
+        // Check if user already exists
+
+        const usersData = JSON.parse(localStorage.getItem("usersData")) || [];
+
+        const existingUser = usersData.find((user) => user.username === username);
+
+
+
+
+        if (existingUser) {
+
+            alert("User already exists. Please login.");
+
+            return;
+
+        }
+
+
+
+
+        // Register new user
+
+
+
+
+        const newUser = {
+
+            username,
+
+            password,
+
+            name,
+
+        };
+
+       
+
+        usersData.push(newUser);
+
+        localStorage.setItem("usersData", JSON.stringify(usersData));
+
+       
+
+
+
+
+        alert("Registration successful. Please login.");
+
+        setUsername("");
+
+        setPassword("");
+
+        setName("");
+
+    };
+
+   
+
+   
+
+    const link = [
+
+        { name: "Login", path: "/login" },
+
+        { name: "Register", path: "/" },
+
+    ]
+
+    const title = "Login/Logout"
+
+    return (<>
+
+        <Navbar link={link} title={title} />
+
+        {/* <div className="container customContainer">
+
+
+            <h2 className="heading">Register</h2>
+
+
+            <div className="row">
+
+                <div className="">
+
+                    <form className="formContainer" onSubmit={handleRegister}>
+
+                        <div className="form-group">
+
+                            <label className="form-label">Name:</label>
+
+                            <input
+
+                                type="text"
+
+                                className="form-control"
+
+                                value={name}
+
+                                onChange={handleNameChange}
+
+                                placeholder=""
+
+                            />
+
+                        </div>
+
+
+
+
+                        <div className="form-group">
+
+                            <label className="form-label">Username:</label>
+
+                            <input
+
+                                type="text"
+
+                                className="form-control"
+
+                                value={username}
+
+                                onChange={handleUsernameChange}
+
+                            />
+
+                        </div>
+
+                        <div className="form-group">
+
+                            <label className="form-label">Password:</label>
+
+                            <input
+
+                                type="password"
+
+                                className="form-control"
+
+                                value={password}
+
+                                onChange={handlePasswordChange}
+
+                                placeholder=""
+
+                            />
+
+                        </div>
+
+
+
+
+                        <button type="submit" className="btn btn-primary customButton">
+
+                            Register
+
+                        </button>
+
+                        <br />
+
+                        <br />
+
+                        <p className="notedText">
+
+                            <span className="alreadyUserText">Already a user? </span> <a className="nottedLink" href="/login">Login</a>
+
+                        </p>
+
+                    </form>
+
+                </div>
+
+            </div>
+
+        </div> */}
+
+<section class="gradient-custom" >
+  <div class="container py-2">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-dark text-white" >
+          <div class="card-body text-center">
+
+            <div>
+
+              <h2 class="fw-bold mb-2 text-uppercase">Register</h2>
+              <p class="text-white-50 mb-5">Please enter your details!</p>
+
+              <div class="form-outline form-white mb-4">
+                <label class="form-label" for="typeEmailX" value={name} onChange={handleNameChange} autoComplete="off">Name</label>
+                <input type="name" id="typeNameX" class="form-control form-control-lg "/>  
+              </div>
+
+              <div class="form-outline form-white mb-4">
+                <label class="form-label" for="typeEmailX" value={username} onChange={handleUsernameChange} autoComplete="off">UserName</label>
+                <input type="name" id="typeEmailX" class="form-control form-control-lg"/>
+              </div>
+              <div class="form-outline form-white mb-4">
+                <label class="form-label" for="typePasswordX" value={password} onChange={handlePasswordChange} autoComplete="off">Password</label>
+                <input type="password" id="typePasswordX" class="form-control form-control-lg"/>   
+              </div>
+
+              <button class="btn btn-outline-light btn-lg px-5" type="submit" onClick={handleRegister}>Register</button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+    </>
+
+    );
+
+
+
+
+}
---
+++ src/Components/Logout.jsx
@@ -0,0 +1,62 @@
+import React,{useState,useEffect} from 'react'
+
+import { useNavigate } from 'react-router-dom';
+
+export default function Logout({onLogOut}) {
+
+    const navigate = useNavigate();
+
+  const [countdown, setCountdown] = useState(5);
+
+  useEffect(() => {
+
+    // Perform logout logic
+
+    onLogOut()
+
+
+
+
+    // Start the countdown timer
+
+    const timer = setInterval(() => {
+
+      setCountdown((prevCountdown) => prevCountdown - 1);
+
+    }, 1000);
+
+
+
+
+    // Redirect to login page after the countdown reaches 0
+
+    if (countdown === 0) {
+
+      navigate("/login");
+
+    }
+
+
+
+
+    return () => {
+
+      clearInterval(timer);
+
+    };
+
+  }, [navigate,onLogOut, countdown]);
+
+  return (
+
+    <div>
+
+      <h1>Logged Out successfully</h1>
+
+      <p>You are redirecting to Log in Page in {countdown} seconds</p>
+
+    </div>
+
+  )
+
+}
