176d744903ed721602542e129a3569338a5dc1b3
Suhani Pachouri <Suhani.Pachouri@Geminisolutions.com>
2023-06-22 17:16:02 +0530

    NaBar and View page improved

--- src/Components/CustomNavBar/Navbar.jsx
+++ src/Components/CustomNavBar/Navbar.jsx
@@ -1,113 +1,40 @@
-// import React from 'react'
-
-// import { Link } from 'react-router-dom';
-
-// export default function Navbar(props) {
-
-//     return (
-
-//         <div>
-
-//           <nav className="nav">
-
-//             <div className="container">
-
-//               <div className="logo">
-
-//                 <h2 className="navbarTitle">{props.title}</h2>
-
-//               </div>
-
-//               <div id="mainListDiv" className="main_list">
-
-//                 <div className="navlinks">
-
-//                   {props.link && props.link.map((link, index) => (
-
-//                     <div key={index}>
-
-//                         <Link to={link.path}>{link.name}</Link>
-
-//                     </div>
-
-//                   ))}
-
-//                 </div>        
-
-//               </div>
-
-//             </div>
-
-//           </nav>
-
-//         </div>
-
-//       );
-
-// }
-
 import Container from 'react-bootstrap/Container';
-
 import Nav from 'react-bootstrap/Nav';
-
 import Navbar from 'react-bootstrap/Navbar';
-
 import { Link } from 'react-router-dom';
-
-// import "./App.css"
-
 function NavScrollExample(props) {
-
+  const handleSearchOnchange=(e)=>{
+    props.searchFunction(e.target.value)
+   }
   return (
-
     <Navbar expand="lg" className="bg-body-tertiary ">
-
       <Container fluid>
-
         <Navbar.Brand href="#">Configuration Board</Navbar.Brand>
-
         <Navbar.Toggle aria-controls="navbarScroll" className='navbarScroll'/>
-
         <Navbar.Collapse id="navbarScroll">
-
           <Nav
-
             className="me-auto my-2 my-lg-0"
-
             style={{ maxHeight: '100px' }}
-
             navbarScroll
-
           >
-
             <Nav.Link >{props.title}</Nav.Link>
-
             <div className="navlinks ">
-
                {props.link && props.link.map((link, index) => (
-
                     <div key={index} className='navlabels'>
                         <span><Link to={link.path + "   "}>{link.name}</Link></span>
-                        
-
                      </div>
-
                    ))}
-
+                 {props.navWithSearch ? (<>
+                  <div>
+                  <input type="text" name="Search Bar" id="" onChange={handleSearchOnchange}/>
+                  </div>
+                 </>):("")}
                  </div>  
-
-          </Nav>
-
-         
-
-        </Navbar.Collapse>
-
+          </Nav>   
+      </Navbar.Collapse>
       </Container>
-
     </Navbar>
-
   );
-
 }
 
 
